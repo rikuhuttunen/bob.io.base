5e91479d46fe92383221dd01f6fef85464a862ed
Manuel Guenther <manuel.guenther@idiap.ch>
2015-04-30 15:49:15 +0200

    Based get_config on bob.extension

--- bob/io/base/__init__.py
+++ bob/io/base/__init__.py
@@ -197,22 +197,7 @@ open = File
 def get_config():
   """Returns a string containing the configuration information.
   """
-
-  import pkg_resources
-  from .version import externals
-
-  packages = pkg_resources.require(__name__)
-  this = packages[0]
-  deps = packages[1:]
-
-  retval =  "%s: %s [api=0x%04x] (%s)\n" % (this.key, this.version,
-      __api_version__, this.location)
-  retval += "  - c/c++ dependencies:\n"
-  for k in sorted(externals): retval += "    - %s: %s\n" % (k, externals[k])
-  retval += "  - python dependencies:\n"
-  for d in deps: retval += "    - %s: %s (%s)\n" % (d.key, d.version, d.location)
-
-  return retval.strip()
+  return bob.extension.get_config(__name__, version.externals, version.api)
 
 
 def get_include_directories():
